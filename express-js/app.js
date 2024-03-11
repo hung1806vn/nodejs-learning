1de06e73ab3e45954ffc9615dee38b152c90f320
DESKTOP-B9M09CM\MyHao <haotruong@javis.vn>
2024-03-11 22:07:25 +0700

    formdata implement, debug and nodemon

--- express-js/app.js
+++ express-js/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const formidableMiddleware = require('express-formidable');
 
 const app = express()
 
 //middleware parse req body to json
-app.use(express.json())
+// app.use(express.json())
+app.use(formidableMiddleware());
 
 app.use('/web18', (req, res, next) => {
     /*
@@ -12,8 +14,11 @@ app.use('/web18', (req, res, next) => {
         "name": "hung"
     }
     */
-    const user = req.body;
+    // const user = req.body;
+    // const user = req.fields;
+    const user = {...req.fields, approve : true};
     if(user.age && user.age > 18){
+        req.fields.approve = false;
         next();
     } else {
         res.status(400).send('Bạn không đủ 18 tuổi');
@@ -21,13 +26,19 @@ app.use('/web18', (req, res, next) => {
 })
 
 app.post('/web18', (req, res) => {
-    const user = req.body
+    const user = req.fields
     res.send(user);
 })
 
 app.post('/web18/haha', (req, res) => {
     const user = req.body
     res.send(user);
+})
+
+app.post('/formdata', (req, res) => {
+    req.fields;
+    console.log(req.fields);
+    res.send(req.fields);
 })
 
 // app.get('/', (req, res) => {
